05fe4c4f553a5315a0550aa65b424270327cf8ad
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-08-12 23:22:30 -0300

    Implementação do projeto no JS

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 const list = document.querySelector('ul')
-const btnMostrarTudo = document.querySelector('#mostar-tudo')
-let myLi = '' //Variavel li começa vazia. 
+const btnMostrarTudo = document.getElementById('mostar-tudo')
+const btnMaperTudo = document.getElementById('maper-tudo')
+const btnSomarValue = document.getElementById('somar-value')
 
-function mostrarTudo() {
-    menuOptions.forEach(product => { //Guardar as li
+//Apresentar os itens na tela.
+function mostrarTudo(myArrayProduct) {
+    let myLi = '' //Variavel li começa vazia. E permanece quando for trocar um dado
+    myArrayProduct.forEach(product => { //Guardar as li
         myLi = myLi + `
             <li>
                 <img src=${product.src}>
@@ -17,5 +20,33 @@ function mostrarTudo() {
     list.innerHTML = myLi // Moastra na tela, todos os produtos.
 }
 
-//Clicar no botão, chamar a função mostarTudo
-btnMostrarTudo.addEventListener("click", mostrarTudo)
+//Colocar 10% de desconto em cado produto, mapeado.
+function maperTudo() {
+    const newPrice = menuOptions.map((product) =>({
+        ...product, // Operador Spread, pega todos que esta em product e coloca dentro da variavel. Mantendo os orginais e alterado o price.  
+        price: product.price * 0.9, //10% de desconto en cada produto
+    }))
+
+    mostrarTudo(newPrice) //Função para mostar na tela
+}
+
+function somarValue(){
+    const valueTotal = menuOptions.reduce((acc, curr) => {
+        return acc + curr.price
+    }, 0)
+    
+    //Apresentar valores na tela
+    list.innerHTML = `
+        <li>
+            <p> Valor total dos Produtos: R$ ${valueTotal.toFixed(2)}</p>
+        </li>
+    `
+
+    //acc - Acumulador
+    //curr - Valor corrente / Atual
+}
+
+//Evendo, quando clicar no botão chamas as funções
+btnMostrarTudo.addEventListener("click", () => mostrarTudo(menuOptions)) //Todo vez que precisar passar um dado detro do addEventListener é preciso colocar a função anonina na frente'() =>'.
+btnMaperTudo.addEventListener("click", maperTudo)
+btnSomarValue.addEventListener("click", somarValue)
